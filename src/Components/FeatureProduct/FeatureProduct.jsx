af0e123f0cb9271befd38a3fa481954bba45dc46
Merna Bassem <mernabassem21@gmail.com>
2024-02-26 00:32:54 +0200

    some change in wish list

--- src/Components/FeatureProduct/FeatureProduct.jsx
+++ src/Components/FeatureProduct/FeatureProduct.jsx
@@ -1,131 +1,4 @@
-// import React, { useContext, useState, useEffect } from "react";
-// import axios from "axios";
-// import Loading from "../Loading/Loading";
-// import { useQuery } from "@tanstack/react-query";
-// import { Link } from "react-router-dom";
-// import toast from "react-hot-toast";
-// import { CartContext } from "../../Context/cartContent";
-// import { AddToWishList, RemoveProductWishList ,GetWishList} from "../../Context/whishListContent";
 
-// export default function FeatureProduct() {
-//   const { AddToCart, setNumOfCartItems } = useContext(CartContext);
-//   const { data, isLoading } = useQuery({
-//     queryKey: "products",
-//     queryFn: getProduct,
-//   });
-
-//   const [wishlist, setWishlist] = useState([]);
-
-//   async function getwishDetail() {
-//     try {
-//       const { data } = await GetWishList();
-//       if (Array.isArray(data)) {
-//         setWishlist(data);
-//       } else {
-//         console.error("Invalid wishlist data:", data);
-//         // setWishlist([]);
-//       }
-//     } catch (error) {
-//       console.error("Error fetching wishlist details:", error);
-//     }
-//   }
-//   async function addCart(id) {
-//     try {
-//       const response = await AddToCart(id);
-//       toast.success("Product Added Successfully");
-//       setNumOfCartItems(response.data.numOfCartItems);
-//     } catch (error) {
-//       toast.error("Product can not be added");
-//     }
-//   }
-
-//   async function addWhichList(id) {
-//     try {
-//       const response = await AddToWishList(id);
-//       toast.success("Product Added to Favorites");
-//       setWishlist([...wishlist, id]);
-//     } catch (error) {
-//       toast.error("Product can not be added");
-//     }
-//   }
-
-//   async function removeWhichList(id) {
-//     try {
-//       await RemoveProductWishList(id);
-//       setWishlist(wishlist.filter((productId) => productId !== id));
-//       toast.success("Product Removed Successfully");
-//     } catch (error) {
-//       toast.error("Product can not be removed");
-//     }
-//   }
-
-//   function getProduct() {
-//     return axios.get(`https://ecommerce.routemisr.com/api/v1/products`);
-//   }
-
-//   useEffect(() => {
-//     getwishDetail();
-//   }, []);
-//   return (
-//     <>
-//       <div className="container mb-5 pb-5 overflow-hidden">
-//         {isLoading ? (
-//           <Loading />
-//         ) : (
-//           <div className="row gy-5">
-//             {data?.data?.data.map((pro) => (
-//               <div key={pro._id} className="col-md-3 cursor-pointer">
-//                 <div className="product py-3 px-2">
-//                   <Link
-//                     to={`/detail/${pro._id}`}
-//                     className="text-decoration-none text-dark"
-//                   >
-//                     <img
-//                       src={pro.imageCover}
-//                       className="w-100"
-//                       alt={pro.title}
-//                     />
-//                     <h5 className="text-center text-main pt-3 pb-2">
-//                       {pro.category.name}
-//                     </h5>
-//                     <h3 className="h6 text-center pb-3">
-//                       {pro.title.split(" ").slice(0, 2).join(" ")}
-//                     </h3>
-//                     <div className="d-flex justify-content-between mx-5 align-items-center">
-//                       <p>{pro.price} EGP</p>
-//                       <p>
-//                         <i className="fa-solid fa-star rating-color"></i>
-//                         {pro.ratingsAverage}
-//                       </p>
-//                     </div>
-//                   </Link>
-//                   <div className="d-flex justify-content-end mx-4 my-3">
-//                     <i
-//                       className={`fa-solid fa-heart fs-2 heart-icon  ${
-//                         wishlist.includes(pro._id) ? "text-danger" : ""
-//                       }`}
-//                       onClick={() =>
-//                         wishlist.includes(pro._id)
-//                           ? removeWhichList(pro._id)
-//                           : addWhichList(pro._id)
-//                       }
-//                     ></i>
-//                   </div>
-//                   <button
-//                     onClick={() => addCart(pro._id)}
-//                     className="btn my-2 w-100 bg-main fs-5 py-2 text-light"
-//                   >
-//                     + Add
-//                   </button>
-//                 </div>
-//               </div>
-//             ))}
-//           </div>
-//         )}
-//       </div>
-//     </>
-//   );
-// }
 import React, { useContext, useState, useEffect } from "react";
 import axios from "axios";
 import Loading from "../Loading/Loading";
@@ -148,7 +21,6 @@ export default function FeatureProduct() {
 
   const [wishlist, setWishlist] = useState([]);
 
-  // Function to fetch wishlist details
   async function getwishDetail() {
     try {
       const { data } = await GetWishList();
@@ -162,7 +34,6 @@ export default function FeatureProduct() {
     }
   }
 
-  // Function to handle adding a product to the cart
   async function addCart(id) {
     try {
       const response = await AddToCart(id);
@@ -173,19 +44,17 @@ export default function FeatureProduct() {
     }
   }
 
-  // Function to handle adding a product to the wishlist
   async function addWhichList(id) {
     try {
       const response = await AddToWishList(id);
       toast.success("Product Added to Favorites");
       setWishlist([...wishlist, id]);
-      localStorage.setItem("wishlist", JSON.stringify([...wishlist, id])); // Store wishlist in local storage
+      localStorage.setItem("wishlist", JSON.stringify([...wishlist, id])); 
     } catch (error) {
       toast.error("Product can not be added");
     }
   }
 
-  // Function to handle removing a product from the wishlist
   async function removeWhichList(id) {
     try {
       await RemoveProductWishList(id);
@@ -193,23 +62,19 @@ export default function FeatureProduct() {
       localStorage.setItem(
         "wishlist",
         JSON.stringify(wishlist.filter((productId) => productId !== id))
-      ); // Update wishlist in local storage
+      ); 
       toast.success("Product Removed Successfully");
     } catch (error) {
       toast.error("Product can not be removed");
     }
   }
 
-  // Function to fetch products
   function getProduct() {
     return axios.get(`https://ecommerce.routemisr.com/api/v1/products`);
   }
 
-  // Effect hook to fetch wishlist details when the component mounts
   useEffect(() => {
     getwishDetail();
-
-    // Retrieve wishlist from local storage when component mounts
     const storedWishlist = JSON.parse(localStorage.getItem("wishlist"));
     if (storedWishlist) {
       setWishlist(storedWishlist);
--- src/Components/WishList/WishList.jsx
+++ src/Components/WishList/WishList.jsx
@@ -1,3 +1,110 @@
+// import React, { useContext, useEffect, useState } from "react";
+// import { CartContext } from "../../Context/cartContent";
+// import Loading from "../Loading/Loading";
+// import toast from "react-hot-toast";
+// import { Link } from "react-router-dom";
+// import { Helmet } from "react-helmet";
+// import { RemoveProductWishList, GetWishList, WhishListContent } from "../../Context/whishListContent";
+
+// export default function WishList() {
+//   let { AddToCart, setNumOfCartItems } = useContext(CartContext);
+//   const [wishDetail, setwishDetail] = useState({});
+//   const [isLoading, setIsLoading] = useState(true);
+  
+//   async function addCart(id) {
+//     let response = await AddToCart(id);
+//     if (response.data.status === "success") {
+//       toast.success("Product Added Successfully");
+//       setNumOfCartItems(response.data.numOfCartItems);
+//       getwishDetail()
+//     } else {
+//       toast.error("Product can not be added");
+//     }
+//   }
+
+//   async function getwishDetail() {
+//     try {
+//       let { data } = await GetWishList();
+//       setwishDetail(data);
+//       setIsLoading(false);
+//     } catch (error) {
+//       console.error("Error fetching cart details:", error);
+//       setIsLoading(false);
+//     }
+//   }
+
+//   async function removeWhichList(id) {
+//     let { data } = await RemoveProductWishList(id);
+//     toast.success("Product Removed Successfully");
+//     getwishDetail()
+//   }
+
+//   useEffect(() => {
+//     getwishDetail();
+//   }, []);
+
+//   return (
+//     <>
+//       <Helmet>
+//         <title>Cart</title>
+//       </Helmet>
+//       {isLoading ? (
+//         <Loading />
+//       ) : (
+//         wishDetail?.data? (
+//           <>
+//             <div className="container my-5 pb-5 overflow-hidden">
+//               <div className="mx-auto bg-main-light py-3 px-4">
+//                 <h1 className="fw-bold">WishList</h1>
+              
+//                 {wishDetail.data.map((pro) => (
+//                   <div className="row px-2 border-bottom py-3" key={pro._id}>
+//                     <div className="col-md-1">
+//                       <img src={pro.imageCover} alt="image" className="w-100" />
+//                     </div>
+//                     <div className="col-md-11">
+//                       <div className="d-flex justify-content-between align-items-center">
+//                         <div>
+//                           <h3 className="fw-bold">{pro.title}</h3>
+//                           <h5 className="text-main fw-bold">{pro.price} EGP</h5>
+//                           <button
+//                             onClick={() => removeWhichList(pro._id)}
+//                             className="text-danger bg-transparent border-0  fw-bold"
+//                           >
+//                             <i className="fa-solid fa-trash-can me-2"></i>
+//                             Remove
+//                           </button>
+//                         </div>
+//                         <div>
+//                           <button
+//                             className="btn bg-main text-white my-3 fs-3 text-center"
+//                             onClick={async () => {
+//                               await RemoveProductWishList(pro._id);
+//                               addCart(pro._id);
+//                             }}
+//                           >
+//                             Add To Cart
+//                           </button>
+//                         </div>
+//                       </div>
+//                     </div>
+//                   </div>
+//                 ))}
+//               </div>
+//             </div>
+//           </>
+//         ) : (
+//           <div className="container">
+//             <div className="bg-main-light mx-auto w-50 text-white p-5 my-5 text-center">
+//               <h2 className="text-main fs-1 fw-bold"> Wish List is Empty</h2>
+//             </div>
+//           </div>
+//         )
+//       )}
+//     </>
+//   );
+// }
+
 import React, { useContext, useEffect, useState } from "react";
 import { CartContext } from "../../Context/cartContent";
 import Loading from "../Loading/Loading";
@@ -7,99 +114,118 @@ import { Helmet } from "react-helmet";
 import { RemoveProductWishList, GetWishList, WhishListContent } from "../../Context/whishListContent";
 
 export default function WishList() {
-  let { AddToCart, setNumOfCartItems } = useContext(CartContext);
+  const { AddToCart, setNumOfCartItems } = useContext(CartContext);
   const [wishDetail, setwishDetail] = useState({});
   const [isLoading, setIsLoading] = useState(true);
-  
+  const [wishlist, setWishlist] = useState([]);
+
+  useEffect(() => {
+    const savedWishlist = JSON.parse(localStorage.getItem("wishlist"));
+    if (savedWishlist) {
+      setWishlist(savedWishlist);
+    }
+    getwishDetail();
+  }, []);
+
   async function addCart(id) {
-    let response = await AddToCart(id);
-    if (response.data.status === "success") {
-      toast.success("Product Added Successfully");
-      setNumOfCartItems(response.data.numOfCartItems);
-      getwishDetail()
-    } else {
+    try {
+      const response = await AddToCart(id);
+      if (response.data.status === "success") {
+        toast.success("Product Added Successfully");
+        setNumOfCartItems(response.data.numOfCartItems);
+        const updatedWishlist = wishlist.filter(productId => productId !== id);
+        setWishlist(updatedWishlist);
+        // removeWhichList(id)
+        RemoveProductWishList(id)
+        localStorage.setItem("wishlist", JSON.stringify(updatedWishlist));
+        getwishDetail();
+      } else {
+        toast.error("Product can not be added");
+      }
+    } catch (error) {
+      console.error("Error adding product to cart:", error);
       toast.error("Product can not be added");
     }
   }
 
   async function getwishDetail() {
     try {
-      let { data } = await GetWishList();
+      const { data } = await GetWishList();
       setwishDetail(data);
       setIsLoading(false);
     } catch (error) {
-      console.error("Error fetching cart details:", error);
+      console.error("Error fetching wishlist details:", error);
       setIsLoading(false);
     }
   }
 
   async function removeWhichList(id) {
-    let { data } = await RemoveProductWishList(id);
-    toast.success("Product Removed Successfully");
-    getwishDetail()
+    try {
+      await RemoveProductWishList(id);
+      const updatedWishlist = wishlist.filter(productId => productId !== id);
+      setWishlist(updatedWishlist);
+      localStorage.setItem("wishlist", JSON.stringify(updatedWishlist));
+      toast.success("Product Removed Successfully");
+      getwishDetail();
+    } catch (error) {
+      console.error("Error removing product from wishlist:", error);
+      toast.error("Product can not be removed");
+    }
   }
-
-  useEffect(() => {
-    getwishDetail();
-  }, []);
 
   return (
     <>
       <Helmet>
-        <title>Cart</title>
+        <title>Wishlist</title>
       </Helmet>
       {isLoading ? (
         <Loading />
       ) : (
-        wishDetail?.data? (
-          <>
-            <div className="container my-5 pb-5 overflow-hidden">
-              <div className="mx-auto bg-main-light py-3 px-4">
-                <h1 className="fw-bold">WishList</h1>
-              
-                {wishDetail.data.map((pro) => (
-                  <div className="row px-2 border-bottom py-3" key={pro._id}>
-                    <div className="col-md-1">
-                      <img src={pro.imageCover} alt="image" className="w-100" />
-                    </div>
-                    <div className="col-md-11">
-                      <div className="d-flex justify-content-between align-items-center">
-                        <div>
-                          <h3 className="fw-bold">{pro.title}</h3>
-                          <h5 className="text-main fw-bold">{pro.price} EGP</h5>
-                          <button
-                            onClick={() => removeWhichList(pro._id)}
-                            className="text-danger bg-transparent border-0  fw-bold"
-                          >
-                            <i className="fa-solid fa-trash-can me-2"></i>
-                            Remove
-                          </button>
-                        </div>
-                        <div>
-                          <button
-                            className="btn bg-main text-white my-3 fs-3 text-center"
-                            onClick={async () => {
-                              await RemoveProductWishList(pro._id);
-                              addCart(pro._id);
-                            }}
-                          >
-                            Add To Cart
-                          </button>
-                        </div>
+        <div className="container my-5 pb-5 overflow-hidden">
+          <div className="mx-auto bg-main-light py-3 px-4">
+      
+            {wishDetail?.data && wishDetail.data.length > 0 ? (
+              <>
+             
+                    <h1 className="fw-bold">Wishlist</h1>
+             { wishDetail.data.map((pro) => (
+                <div className="row px-2 border-bottom py-3" key={pro._id}>
+                  <div className="col-md-1">
+                    <img src={pro.imageCover} alt="image" className="w-100" />
+                  </div>
+                  <div className="col-md-11">
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div>
+                        <h3 className="fw-bold">{pro.title}</h3>
+                        <h5 className="text-main fw-bold">{pro.price} EGP</h5>
+                        <button
+                          onClick={() => removeWhichList(pro._id)}
+                          className="text-danger bg-transparent border-0  fw-bold"
+                        >
+                          <i className="fa-solid fa-trash-can me-2"></i>
+                          Remove
+                        </button>
+                      </div>
+                      <div>
+                        <button
+                          className="btn bg-main text-white my-3 fs-3 text-center"
+                          onClick={() => addCart(pro._id)}
+                        >
+                          Add To Cart
+                        </button>
                       </div>
                     </div>
                   </div>
-                ))}
+                </div>
+              ))}
+              </>
+            ) : (
+              <div className="bg-main-light mx-auto  text-white p-5 my-5 text-center">
+                <h2 className="text-main fs-1 fw-bold">Wishlist is Empty</h2>
               </div>
-            </div>
-          </>
-        ) : (
-          <div className="container">
-            <div className="bg-main-light mx-auto w-50 text-white p-5 my-5 text-center">
-              <h2 className="text-main fs-1 fw-bold"> Wish List is Empty</h2>
-            </div>
+            )}
           </div>
-        )
+        </div>
       )}
     </>
   );
