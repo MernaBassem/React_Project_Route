b929f3040a5615ba4c3ac25555709f698452f36a
Merna Bassem <mernabassem21@gmail.com>
2024-02-27 03:01:01 +0200

    change in footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,19 +4,31 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <>
-      <footer className="text-center bg-main-light fixed-bottom  p-3">
-                       
-          <div>
-            <h3>
-              {/* Get the FreshCart app */}
-              Footer
-            </h3>
-            <p>
+      <footer className="bg-main-light fixed-bottom  p-5">
+        <div>
+          <h2 className="fw-bold py-2 ">
+            Get the FreshCart app
+           
+          </h2>
+          <p className="fs-5 py-2 ">
+            We will send you a link , open it on your phoneto download the app
+          </p>
 
-            </p>
+          <div className="row gy-3">
+            <div className="col-md-9">
+              <input
+                type="email"
+                placeholder="Email"
+                className="py-2 w-100 rounded  px-4"
+              />
+            </div>
+            <div className="col-md-3">
+              <button className="btn bg-main py-2 fs-5 text-light px-3  ">
+                Share App Link
+              </button>
+            </div>
           </div>
-
-
+        </div>
       </footer>
     </>
   );
